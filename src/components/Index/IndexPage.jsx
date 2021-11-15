0da9b3c498d25223654cf24f6f2832aafaf8ce91
Lee Chuan Xin <chuanxin@gametize.com>
2021-11-16 07:59:06 +0800

    feat: create trip button.

--- src/components/Index/IndexPage.jsx
+++ src/components/Index/IndexPage.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types, jsx-a11y/label-has-associated-control */
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 
 export default function IndexPage({
   isLoggedIn,
@@ -12,8 +12,8 @@ export default function IndexPage({
   return (
     <div className="container-fluid pt-5">
       <div className="row w-100 pt-3">
-        <div className="col-12 pt-1">
-          Logged in!
+        <div className="col-8 pt-1 ms-auto me-auto">
+          <Link class="btn btn-primary btn-lg w-100" to="/createtrip" role="button">+ Create a Trip</Link>
         </div>
       </div>
     </div>
